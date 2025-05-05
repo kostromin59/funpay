df1818ec1d0c444a49db42a4674107a5cf5c639e
kostromin59 <ngskostromin@gmail.com>
2025-05-05 19:20:42 +0500

    feat: app data model

--- funpay.go
+++ funpay.go
@@ -7,3 +7,10 @@ const (
 	// FunpayURL is the base URL for the Funpay website.
 	FunpayURL = "https://" + FunpayDomain
 )
+
+// AppData represents the object from data-app-data attribute inside the body element.
+type AppData struct {
+	CSRFToken string `json:"csrf-token,omitempty"`
+	UserID    int64  `json:"userId,omitempty"`
+	Locale    string `json:"locale,omitempty"`
+}
