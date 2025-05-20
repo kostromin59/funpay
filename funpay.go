6ca63904890b00fde7b3e8ed2293820434c5ef6e
kostromin59 <ngskostromin@gmail.com>
2025-05-20 12:47:31 +0500

    refactor: reorder methods

--- funpay.go
+++ funpay.go
@@ -40,39 +40,6 @@ func New(goldenKey, userAgent string) *Funpay {
 	}
 }
 
-// Update calls [Funpay.RequestHTML]. You should call it every 40-60 minutes to update PHPSESSIONID cookie.
-// [Funpay.Request] saves all cookies from response if they are not empty.
-func (fp *Funpay) Update(ctx context.Context) error {
-	const op = "Funpay.Update"
-
-	_, err := fp.RequestHTML(ctx, fp.baseURL)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
-
-	return nil
-}
-
-// UpdateLocale calls [Funpay.RequestHTML] with setlocale query param.
-func (fp *Funpay) UpdateLocale(ctx context.Context, locale Locale) error {
-	const op = "Funpay.UpdateLocale"
-
-	reqURL, err := url.Parse(fp.baseURL)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
-
-	q := reqURL.Query()
-	q.Set("setlocale", string(locale))
-	reqURL.RawQuery = q.Encode()
-
-	if _, err := fp.RequestHTML(ctx, reqURL.String()); err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
-
-	return nil
-}
-
 // Account returns account info: id, username, balance, locale. [Funpay.RequestHTML] updates account info.
 func (fp *Funpay) Account() *account {
 	return fp.account
@@ -111,6 +78,145 @@ func (fp *Funpay) SetProxy(proxy *url.URL) {
 	fp.mu.Lock()
 	fp.proxy = proxy
 	fp.mu.Unlock()
+}
+
+// Update calls [Funpay.RequestHTML]. You should call it every 40-60 minutes to update PHPSESSIONID cookie.
+// [Funpay.Request] saves all cookies from response if they are not empty.
+func (fp *Funpay) Update(ctx context.Context) error {
+	const op = "Funpay.Update"
+
+	_, err := fp.RequestHTML(ctx, fp.baseURL)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+// UpdateLocale calls [Funpay.RequestHTML] with setlocale query param.
+func (fp *Funpay) UpdateLocale(ctx context.Context, locale Locale) error {
+	const op = "Funpay.UpdateLocale"
+
+	reqURL, err := url.Parse(fp.baseURL)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	q := reqURL.Query()
+	q.Set("setlocale", string(locale))
+	reqURL.RawQuery = q.Encode()
+
+	if _, err := fp.RequestHTML(ctx, reqURL.String()); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+// UpdateLots updates lots for current account. Use Funpay.Lots().List() to get lots.
+// Returns [ErrAccountUnauthorized] if user id equals 0. Call [Funpay.Update] to update account info.
+func (fp *Funpay) UpdateLots(ctx context.Context) error {
+	const op = "Funpay.UpdateLots"
+
+	id := fp.Account().ID()
+	if id == 0 {
+		return fmt.Errorf("%s: %w", op, ErrAccountUnauthorized)
+	}
+
+	lots, err := fp.LotsByUser(context.Background(), id)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	fp.Lots().updateList(lots)
+
+	return nil
+}
+
+// LotsByUser loads lots for provided userID.
+func (fp *Funpay) LotsByUser(ctx context.Context, userID int64) (map[string][]string, error) {
+	const op = "Funpay.LotsByUser"
+
+	reqURL, err := url.Parse(fp.baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	reqURL = reqURL.JoinPath("users", fmt.Sprintf("%d", userID), "/")
+
+	doc, err := fp.RequestHTML(ctx, reqURL.String())
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	lots, err := fp.lots.extractLots(doc)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return lots, nil
+}
+
+// LotFields loads [LotFields] for nodeID (category) or offerID. Values will be filled with provided offerID.
+func (fp *Funpay) LotFields(ctx context.Context, nodeID, offerID string) (LotFields, error) {
+	const op = "Funpay.LotFields"
+
+	reqURL, err := url.Parse(fp.baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	reqURL = reqURL.JoinPath("lots", "offerEdit")
+
+	q := reqURL.Query()
+	if offerID != "" {
+		q.Set("offer", offerID)
+	}
+	if nodeID != "" {
+		q.Set("node", nodeID)
+	}
+	reqURL.RawQuery = q.Encode()
+
+	doc, err := fp.RequestHTML(ctx, reqURL.String())
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return fp.lots.extractFields(doc), nil
+}
+
+// SaveLot makes request to /lots/offerSave. Use [Funpay.LotFields] to get fields.
+//
+//	Fields:
+//	- Provide offer_id to update lot;
+//	- Set offer_id = "0" to create lot;
+//	- Set deleted = "1" to delete lot.
+func (fp *Funpay) SaveLot(ctx context.Context, fields LotFields) error {
+	const op = "Funpay.SaveLot"
+
+	body := url.Values{}
+
+	for name, v := range fields {
+		body.Set(name, v.Value)
+	}
+
+	body.Set(FormCSRFToken, fp.CSRFToken())
+	body.Set("location", "trade")
+
+	_, err := fp.Request(ctx, fp.baseURL+"/lots/offerSave",
+		RequestWithMethod(http.MethodPost),
+		RequestWithBody(bytes.NewBufferString(body.Encode())),
+		RequestWithHeaders(map[string]string{
+			"content-type":     "application/x-www-form-urlencoded; charset=UTF-8",
+			"accept":           "*/*",
+			"x-requested-with": "XMLHttpRequest",
+		}),
+	)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
 
 // Request executes an HTTP request using the account's session.
@@ -272,112 +378,6 @@ func (fp *Funpay) updateAppData(doc *goquery.Document) error {
 	fp.mu.Lock()
 	fp.csrfToken = appData.CSRFToken
 	fp.mu.Unlock()
-
-	return nil
-}
-
-// UpdateLots updates lots for current account. Use Funpay.Lots().List() to get lots.
-// Returns [ErrAccountUnauthorized] if user id equals 0. Call [Funpay.Update] to update account info.
-func (fp *Funpay) UpdateLots(ctx context.Context) error {
-	const op = "Funpay.UpdateLots"
-
-	id := fp.Account().ID()
-	if id == 0 {
-		return fmt.Errorf("%s: %w", op, ErrAccountUnauthorized)
-	}
-
-	lots, err := fp.LotsByUser(context.Background(), id)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
-
-	fp.Lots().updateList(lots)
-
-	return nil
-}
-
-// LotsByUser loads lots for provided userID.
-func (fp *Funpay) LotsByUser(ctx context.Context, userID int64) (map[string][]string, error) {
-	const op = "Funpay.LotsByUser"
-
-	reqURL, err := url.Parse(fp.baseURL)
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-
-	reqURL = reqURL.JoinPath("users", fmt.Sprintf("%d", userID), "/")
-
-	doc, err := fp.RequestHTML(ctx, reqURL.String())
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-
-	lots, err := fp.lots.extractLots(doc)
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-
-	return lots, nil
-}
-
-// LotFields loads [LotFields] for nodeID (category) or offerID. Values will be filled with provided offerID.
-func (fp *Funpay) LotFields(ctx context.Context, nodeID, offerID string) (LotFields, error) {
-	const op = "Funpay.LotFields"
-
-	reqURL, err := url.Parse(fp.baseURL)
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-
-	reqURL = reqURL.JoinPath("lots", "offerEdit")
-
-	q := reqURL.Query()
-	if offerID != "" {
-		q.Set("offer", offerID)
-	}
-	if nodeID != "" {
-		q.Set("node", nodeID)
-	}
-	reqURL.RawQuery = q.Encode()
-
-	doc, err := fp.RequestHTML(ctx, reqURL.String())
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-
-	return fp.lots.extractFields(doc), nil
-}
-
-// SaveLot makes request to /lots/offerSave. Use [Funpay.LotFields] to get fields.
-//
-//	Fields:
-//	- Provide offer_id to update lot;
-//	- Set offer_id = "0" to create lot;
-//	- Set deleted = "1" to delete lot.
-func (fp *Funpay) SaveLot(ctx context.Context, fields LotFields) error {
-	const op = "Funpay.SaveLot"
-
-	body := url.Values{}
-
-	for name, v := range fields {
-		body.Set(name, v.Value)
-	}
-
-	body.Set(FormCSRFToken, fp.CSRFToken())
-	body.Set("location", "trade")
-
-	_, err := fp.Request(ctx, fp.baseURL+"/lots/offerSave",
-		RequestWithMethod(http.MethodPost),
-		RequestWithBody(bytes.NewBufferString(body.Encode())),
-		RequestWithHeaders(map[string]string{
-			"content-type":     "application/x-www-form-urlencoded; charset=UTF-8",
-			"accept":           "*/*",
-			"x-requested-with": "XMLHttpRequest",
-		}),
-	)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
 
 	return nil
 }
